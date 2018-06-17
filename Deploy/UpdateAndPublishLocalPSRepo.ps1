b3cd703d4042210551ef4f19c3ed843aab84b21c
Adam Rush <adamrushuk@gmail.com>
2018-06-17 09:52:33 +0100

    Added update and publish module example

---
+++ Deploy/UpdateAndPublishLocalPSRepo.ps1
@@ -0,0 +1,24 @@
+<# This script will:
+- Uninstall all versions of the given module names.
+- Install the latest available modules.
+- Publish the new modules to your PowerShell Repository
+#>
+
+# Vars
+$localPSRepositoryName = 'LocalPSRepository'
+$moduleNames = @('xDscDiagnostics', 'xDSCResourceDesigner')
+
+# Show currently installed module versions
+$moduleNames | ForEach-Object { Get-Module -Name $_ -ListAvailable }
+
+# Uninstall modules
+$moduleNames | ForEach-Object { Uninstall-Module -Name $_ -AllVersions -Force -Verbose }
+
+# Install modules
+Install-Module -Name $moduleNames -Repository 'PSGallery' -AllowClobber -Force -Verbose
+
+# Show currently installed module versions
+$moduleNames | ForEach-Object { Get-Module -Name $_ -ListAvailable }
+
+# Publish multiple modules
+$moduleNames | ForEach-Object { Publish-Module -Name $_ -Repository $localPSRepositoryName -Verbose -Force }

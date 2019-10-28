d36a5ceac1ac62c1a4c6010d1897965396a98e0d
Adam Rush <adamrushuk@gmail.com>
2019-10-28 21:04:42 +0000

    Added PowerShell Core profile

---
+++ Profile/pscore_profile.ps1
@@ -0,0 +1,7 @@
+#requires -module posh-git, oh-my-posh
+Set-Theme Paradox
+
+# Ref: https://www.hanselman.com/blog/HowToMakeAPrettyPromptInWindowsTerminalWithPowerlineNerdFontsCascadiaCodeWSLAndOhmyposh.aspx
+# Rename this file "profile.ps1" and move to:
+#   Windows Path: "C:\Users\$env:USERPROFILE\Documents\PowerShell\profile.ps1"
+#   Linux Path: "$HOME/.config/powershell/profile.ps1"

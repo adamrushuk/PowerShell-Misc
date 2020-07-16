ca0bb8f7edeee486118b9f61b9790cf3ccd370e4
Adam Rush <adamrushuk@gmail.com>
2020-07-16 11:12:39 +0100

    Added more Azure examples

--- Azure/Delete-ResourceGroups.ps1
+++ Azure/Delete-ResourceGroups.ps1
@@ -5,14 +5,15 @@
     Deletes Azure Resource Groups with a given prefix, with confirmation prompt and WhatIf functionality
 .PARAMETER Prefixes
     An array of prefix strings that matches the start of the Resource Group name
-    # "abc99", "abc12" would match resource group called "abc99-rg-blahblah" and "abc12-rg-blahblah"
+    "abc99", "abc12" would match resource group called "abc99-rg-blahblah" and "abc12-rg-blahblah"
+    Wildcards are supported, so you could use "abc*" instead of "abc99" and "abc12".
 .PARAMETER MaxLimit
     Aborts script if too many Resource Groups are found.
-    This is a safety check
+    This is a safety check.
 .PARAMETER WhatIf
-    Does a dry-run and shows what Resource Groups would be deleted
+    Does a dry-run and shows what Resource Groups would be deleted.
 .EXAMPLE
-    ./Delete-ResourceGroups.ps1 -Prefixes abc99
+    ./Delete-ResourceGroups.ps1 -Prefixes abc99.
 
     Deletes all Resource Groups starting with "abc99", eg:
     "abc99-rg-one"
@@ -34,7 +35,7 @@ param (
     $Prefixes,
 
     [int]
-    $MaxLimit = 5,
+    $MaxLimit = 15,
 
     [switch]
     $WhatIf
@@ -49,7 +50,7 @@ $jobs = $null
 foreach ($Prefix in $Prefixes) {
 
     $resourceGroups = $null
-    $resourceGroups = Get-AzResourceGroup -Name "$Prefix*"
+    $resourceGroups = @(Get-AzResourceGroup -Name "$Prefix*")
     Write-Host "`nResource groups found starting with [$Prefix]: [$($resourceGroups.Count)]" -ForegroundColor Yellow
 
     # abort if we find no resource groups
@@ -92,6 +93,7 @@ if ($resourceGroupsToDelete.Count -gt 0) {
     if ($WhatIf.IsPresent) {
         $resourceGroupsToDelete | Remove-AzResourceGroup -Force -WhatIf
     } else {
+        $timer = [Diagnostics.Stopwatch]::StartNew()
         $jobs += $resourceGroupsToDelete | Remove-AzResourceGroup -Force -AsJob
     }
 }
@@ -103,4 +105,7 @@ if ($null -ne $jobs) {
     Write-Output "`nWaiting for [$($jobs.Count)] jobs to finish..."
     $jobs | Wait-Job
     $jobs | Receive-Job -Keep
+
+    $timer.Stop()
+    Write-Output "Deletion jobs completed in: [$($timer.Elapsed.Minutes)m$($timer.Elapsed.Seconds)s]"
 }
